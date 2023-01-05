5fba4363bdc16af1140b57a92ec40d8ae112e9eb
John Hadikusumo <john.hadikusumo@gmail.com>
2023-01-05 16:24:16 +1100

    Add project files.

---
+++ KiwiSoft.MailJet/EMailBroker.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System;
+using System.IO;
+using System.Net.Http;
+using Microsoft.Extensions.Configuration;
+
+namespace KiwiSoft.MailJet
+{
+    public class EMailBroker
+    {
+        private readonly string _apiurl = "ttps://api.mailjet.com/v3.1/send";
+        private readonly string _appsettings = "appsettings.json";
+        private readonly string _section = "KiwiSoft.MailJet";
+        private readonly string? _app_key = string.Empty;
+        private readonly string? _secret_key = string.Empty;
+        public EMailBroker() {
+
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(_appsettings)
+                .Build();
+
+            IConfigurationSection section = config.GetSection(_section);
+            _app_key = section.GetSection("API_KEY").Value;
+            _secret_key = section.GetSection("SECRET_KEY").Value;
+        }
+
+        public EMailBroker(string filesetting)
+        {
+            _appsettings = filesetting;
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(_appsettings)
+                .Build();
+
+            IConfigurationSection section = config.GetSection(_section);
+            _app_key = section.GetSection("API_KEY").Value;
+            _secret_key = section.GetSection("SECRET_KEY").Value;
+        }
+    }
+}
---
+++ MailTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using KiwiSoft.MailJet;
+
+namespace MailTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            MailBroker broker = new MailBroker();
+            Assert.True(true);
+        }
+    }
+}
